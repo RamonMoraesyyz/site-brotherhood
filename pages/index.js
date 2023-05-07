04be3cb8af2dd30921c1b659de6130c5d881da1c
RamonMoraesyyz <ramon.k13@gmail.com>
2023-05-07 20:07:50 -0300

    teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-
+import Link from 'next/link';
 function Home() {
     return (<div>
         <h1>Home</h1>
-        <a href="/home"> Acessar página Sobre </a>
 
+        <link href="/sobre"> 
+            <a>Acessar página Sobre </a>
+        </link>
     </div>)
 }
 export default Home
