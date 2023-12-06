4e5729bd2516ccddb3cace7dc8f8b953714f1fbb
Eduardo Braga <eduardocbraga@hotmail.com>
2023-12-06 15:52:24 -0300

    Substitui links do header por botões do nuxt ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,24 @@
+<script setup>
+const items = [
+  [{
+    label: 'Cerveja 1',
+  }],[{
+    label: 'Cerveja 2',
+  }],[{
+    label: 'Cerveja 3',
+  }],[{
+    label: 'Cerveja 4',
+  }],
+]
+</script>
 <template>
-  <header class="bg-black text-white p-4 flex justify-end">
-    <a href="#" class="mx-2">Início</a>
-    <a href="#" class="mx-2">Sobre Nós</a>
-    <a href="#" class="mx-2">Contato</a>
-    <a href="#" class="mx-2">Produtos</a>
+  <header class="bg-black text-white px-4 flex justify-end">
+    <div class="flex flex-row gap-2">
+      <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Início</UButton>
+      <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Sobre nós</UButton>
+      <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Contato</UButton>
+        <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Produtos</UButton>
+    </UDropdown>
+    </div>
   </header>
 </template>
