9105a40b77d20da800cdd3011122ad134ee829dd
Eduardo Braga <eduardocbraga@hotmail.com>
2024-01-20 22:04:02 -0300

    refatora navbar

--- components/BrotherNavbar.vue
+++ components/BrotherNavbar.vue
@@ -1,29 +1,25 @@
 <template>
-    <header class="bg-black text-white px-4 flex justify-end">
-      <div class="flex flex-row gap-2">
-        <div class="absolute top-1 left-1 text-center ml-3">
     <header class="bg-gray-900 text-white px-4 flex justify-between">
+      <!-- <div class="flex flex-row gap-2"> -->
+        <div class="ml-3">
           <NuxtLink to="/">
           <img src="/favicon.ico" alt="Favicon" class="w-8 h-8" />
           </NuxtLink>
         </div>
+        <div>
         <NuxtLink to="/">
-          <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Início</UButton>
           <UButton color="black" :ui="{ rounded: 'rounded-none' }">Início</UButton>
         </NuxtLink>
         <NuxtLink to="/sobrenos">
-          <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Sobre nós</UButton>
           <UButton color="black" :ui="{ rounded: 'rounded-none' }">Sobre nós</UButton>
         </NuxtLink>
         <NuxtLink to="/contato">
-          <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Contato</UButton>
           <UButton color="black" :ui="{ rounded: 'rounded-none' }">Contato</UButton>
         </NuxtLink>
         <UDropdown :items="dropdownItems" :popper="{ placement: 'bottom-start' }">
-          <UButton color="black" variant="solid" :ui="{ rounded: 'rounded-none' }">Produtos</UButton>
           <UButton color="black" :ui="{ rounded: 'rounded-none' }">Produtos</UButton>
         </UDropdown>
-      </div>
+        </div>
     </header>
   </template>
   
