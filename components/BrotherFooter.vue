fd2f9e78533d79a348b72a1f65e76f73719a5b08
Eduardo Braga <eduardocbraga@hotmail.com>
2024-01-20 22:02:24 -0300

    remove código não necessário

--- components/BrotherFooter.vue
+++ components/BrotherFooter.vue
@@ -1,12 +1,11 @@
 <template>
-  <footer class="fixed bottom-0 w-full bg-gray-950 px-4 flex justify-between items-center text-gray-50">
   <footer class="fixed bottom-0 w-full bg-gray-900 px-4 flex justify-between items-center text-gray-50">
     <div>2023 | Brotherhood bebidas artesanais</div>
     <div class="flex flex-row items-center">
-      <UButton icon="i-ic-sharp-whatsapp" size="sm" color="black" variant="solid" :trailing="false"></UButton>
-      <UButton icon="i-ic-outline-mail" size="sm" color="black" variant="solid" :trailing="false"></UButton>
-      <UButton icon="i-ri-instagram-line" size="sm" color="black" variant="solid" :trailing="false"></UButton>
-      <UButton icon="i-tabler-phone" size="sm" color="black" variant="solid" :trailing="false"></UButton>
+      <UButton icon="i-ic-sharp-whatsapp" size="sm" color="black"></UButton>
+      <UButton icon="i-ic-outline-mail" size="sm" color="black"></UButton>
+      <UButton icon="i-ri-instagram-line" size="sm" color="black"></UButton>
+      <UButton icon="i-tabler-phone" size="sm" color="black"></UButton>
     </div>
   </footer>
 </template>
