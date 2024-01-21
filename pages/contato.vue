bff63be0450da54998f338724df3eb1820b3023d
Eduardo Braga <eduardocbraga@hotmail.com>
2024-01-20 22:18:02 -0300

    ajusta espaçamentos verticais do formulário

--- pages/contato.vue
+++ pages/contato.vue
@@ -4,7 +4,7 @@
         <div class="mb-8">
             <h1 class="text-5xl font-bold py-8">Contato</h1>
             <h2 class="text-lg py-2">Rua Francisco Medeiros, N°243, Centro<br>Bela Vista de Goiás/GO - CEP: 75240-000</h2>
-            <div class="my-7 p-4 h-80 rounded-3xl border-4 border-orange-300 text-xl text-justify relative overflow-hidden">
+            <div class="mt-6 p-4 h-80 rounded-3xl border-4 border-orange-300 text-xl text-justify relative overflow-hidden">
             <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3816.0950052721955!2d-48.95635125352049!3d-16.969876506355227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935f24a3efd5b04f%3A0x52f8564a82c394a!2sR.%20Francisco%20Medeiros%2C%20243%2C%20Bela%20Vista%20de%20Goi%C3%A1s%20-%20GO%2C%2075240-000!5e0!3m2!1spt-BR!2sbr!4v1704590202068!5m2!1spt-BR!2sbr"
             width="600"
@@ -18,7 +18,7 @@
             ></iframe>
             </div>
         </div>
-        <div class="flex flex-col justify-end text-sm font-bold px-4 py-8">
+        <div class="flex flex-col justify-end text-sm font-bold px-4 py-6">
             <UFormGroup class="w-full mb-4">
                 <UInput placeholder="Nome" />
             </UFormGroup>
