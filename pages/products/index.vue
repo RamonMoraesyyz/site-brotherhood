1d9e5a3ce5edbac88f133ed86d5248fcc1c3d6c4
Eduardo Braga <eduardocbraga@hotmail.com>
2024-01-20 22:05:53 -0300

    refatora página de produto

---
+++ pages/products/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex-grow   grid md:grid-cols-2">
+    
+    <div class="flex justify-center items-center">
+      <img src="../../public/jabuticaba.png" alt="Product Image" class="w-1/3 mx-auto mt-12 md:mt-0">
+    </div>
+
+    <div class="flex flex-col p-4 justify-around">
+      
+      <div class="mx-1 my-2 p-8 rounded-3xl border-4 border-gray-800 text-xl text-justify">
+        <h2 class="font-bold text-2xl mb-4">Vinho de Jabuticaba</h2>
+        <p>
+          O vinho de jabuticaba é uma bebida única, destacando-se pela coloração vibrante, sabor agridoce equilibrado com notas frutadas intensas da jabuticaba. Com aroma marcante, teor alcoólico elevado e versatilidade,
+          é apreciado tanto por si só quanto em harmonização com queijos finos e pratos que complementam suas características frutadas.
+          Uma opção exótica para apreciadores de vinhos distintos.
+        </p>
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 text-center">
+        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">ABV: 15%</div>
+        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">IBU: 6</div>
+        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">EBC: 47</div>
+      </div>
+    </div>
+
+  </div>
+</template>
