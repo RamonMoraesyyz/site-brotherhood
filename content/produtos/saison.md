38413c1836ccad7b7154a57a231b54ee04e549f2
RamonMoraesyyz <ramon.k13@gmail.com>
2024-01-24 21:50:26 -0300

    primeiras informações markdown

--- content/produtos/saison.md
+++ content/produtos/saison.md
@@ -1,1 +1,9 @@
-# Brotherhood, a melhor fábrica de bebidas do BR!
+---
+name: 'Saison'
+ibu: '12'
+abv: '8,0%'
+ebc: '8'
+harmonization: 'Queijos, castanhas, frutos do mar, frituras e carne vermelha'
+image: '/images/saison.png'
+---
+Uma cerveja belga bem encorpada, com notas frutadas e condimentadas oriundas da fermentação e adição de especiarias como gengibre e cascas de tangerina, com amargor moderado, acidez leve e final seco.
--- pages/produtos/[...slug].vue
+++ pages/produtos/[...slug].vue
@@ -2,24 +2,33 @@
   <div class="flex-grow grid md:grid-cols-2 text-gray-800">
     
     <div class="flex justify-center items-center">
-      <img src="../../public/jabuticaba.png" alt="Product Image" class="w-1/3 mx-auto mt-12 md:mt-0">
+      <img :src="product.image" />
+      
     </div>
 
     <div class="flex flex-col p-4 justify-around">
       <div class="mx-1 my-2 p-8 rounded-3xl border-4 border-gray-800 text-xl text-justify">
-        <h2 class="font-bold text-2xl mb-4">Vinho de Jabuticaba</h2>
+        <h2 class="font-bold text-2xl mb-4">{{product.name}}</h2>
         <p>
-          O vinho de jabuticaba é uma bebida única, destacando-se pela coloração vibrante, sabor agridoce equilibrado com notas frutadas intensas da jabuticaba. Com aroma marcante, teor alcoólico elevado e versatilidade,
-          é apreciado tanto por si só quanto em harmonização com queijos finos e pratos que complementam suas características frutadas.
-          Uma opção exótica para apreciadores de vinhos distintos.
+          <ContentRenderer :value="product" />
+          {{product.harmonization}}
         </p>
       </div>
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4 text-center">
-        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">ABV: 15%</div>
-        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">IBU: 6</div>
-        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">EBC: 47</div>
+        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">{{product.abv}}</div>
+        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">{{product.ibu}}</div>
+        <div class="p-8 border-4 border-gray-800 font-bold text-xl rounded-3xl">{{product.ebc}}</div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { useRoute } from "nuxt/app";
+
+const route = useRoute()
+const productName = route.params.slug
+
+const { data: product } = await useAsyncData('produtos', () => queryContent(`/produtos/${productName}`).findOne())
+</script>
