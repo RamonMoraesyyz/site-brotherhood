ccaae6c990bbb2fe16387fe6b89b1fd915b9aea4
Eduardo Braga <eduardocbraga@hotmail.com>
2024-02-12 01:13:16 -0300

    corrige video no mobile

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 
     <!-- Vídeo de Fundo -->
     <div class="absolute w-full h-full bg-yellow-500 opacity-50">
-      <video autoplay muted loop class="object-cover w-full h-full">
+      <video preload="auto" webkit-playsinline playsinline autoplay muted loop class="object-cover w-full h-full">
         <source src="/beervideo.mp4" type="video/mp4">
       </video>
     </div>
