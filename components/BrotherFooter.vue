8213e5b49d19ea76fc395c181af4e3159db160cd
Eduardo Braga <eduardocbraga@hotmail.com>
2024-02-13 21:40:27 -0300

    abre os links do footer em uma nova aba

--- components/BrotherFooter.vue
+++ components/BrotherFooter.vue
@@ -2,9 +2,9 @@
   <footer class="fixed bottom-0 w-full bg-gray-900 px-4 flex justify-between items-center text-gray-50">
     <div>2023 | Brotherhood bebidas artesanais</div>
     <div class="flex flex-row items-center">
-      <UButton icon="i-ic-sharp-whatsapp" size="sm" color="black" to="https://wa.me/5562996878970"> </UButton>
+      <UButton icon="i-ic-sharp-whatsapp" size="sm" color="black" to="https://wa.me/5562996878970" target="_blank"> </UButton>
       <UButton icon="i-ic-outline-mail" size="sm" color="black" to="mailto: https://ramon.k13@gmail.com"></UButton>
-      <UButton icon="i-ri-instagram-line" size="sm" color="black" to="https://www.instagram.com/?hl=pt-br"></UButton>
+      <UButton icon="i-ri-instagram-line" size="sm" color="black" to="https://www.instagram.com/?hl=pt-br" target="_blank"></UButton>
       <UButton icon="i-tabler-phone" size="sm" color="black" to="tel:+5562996878970"></UButton>
     </div>
   </footer>
