e39a7f2c41d9d8bb3cb8fc9fd9fc2fe754f9f3f5
Eduardo Braga <eduardocbraga@hotmail.com>
2024-02-15 20:46:36 -0300

    coloca primeiro frame do vídeo como fundo para mostrar algo enquanto o vídeo carrega

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="relative w-full h-screen overflow-hidden">
 
     <!-- Vídeo de Fundo -->
-    <div class="absolute w-full h-full bg-yellow-500 opacity-50">
+    <div class="absolute w-full h-full bg-yellow-500 opacity-50 bg-center bg-cover" style="background-image: url('/beerimage.jpg')">
       <video preload="auto" webkit-playsinline playsinline="true" autoplay muted loop disablepictureinpicture class="object-cover w-full h-full">
         <source src="/beervideo.mp4" type="video/mp4">
       </video>
