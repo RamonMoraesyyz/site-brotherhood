30b4d753621e7d6e3504368ed4fec32549cc28df
Eduardo Braga <eduardocbraga@hotmail.com>
2024-02-15 21:08:35 -0300

    força a reprodução do vídeo

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,28 @@
+<script setup>
+  // Assim que o navegador montar a página ele irá pegar onde o vídeo está e tocar ele.
+
+  onMounted(() => {
+    // Pega onde o vídeo está
+    const video = document.getElementById('backgroundVideo');
+
+    // Define uma função para iniciar a reprodução do vídeo:
+    const playVideo = () => {
+      video.play().catch(error => {
+        console.error('Erro ao reproduzir o vídeo:', error);
+      });
+    };
+
+    // Chama a função definida anteriormente para iniciar o vídeo
+    playVideo();
+  })
+</script>
+
 <template>
   <div class="relative w-full h-screen overflow-hidden">
 
     <!-- Vídeo de Fundo -->
     <div class="absolute w-full h-full bg-yellow-500 opacity-50 bg-center bg-cover" style="background-image: url('/beerimage.jpg')">
-      <video preload="auto" webkit-playsinline playsinline="true" autoplay muted loop disablepictureinpicture class="object-cover w-full h-full">
+      <video id="backgroundVideo" preload="auto" webkit-playsinline playsinline autoplay muted loop disablepictureinpicture class="object-cover w-full h-full">
         <source src="/beervideo.mp4" type="video/mp4">
       </video>
     </div>
